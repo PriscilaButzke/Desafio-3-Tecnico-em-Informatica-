c5254ef485830af980d035e7724b31b5868e141d
PriscilaButzke <priscilabutzke@outlook.com>
2022-03-17 12:40:54 -0300

    Versão 1 Scanner
    Versão 1 Scanner

---
+++ src/pesoIdeal/Peso.java
@@ -0,0 +1,95 @@
+package pesoIdeal;
+
+/**
+ *
+ * @author Priscila Butzke
+ */
+public class Peso {
+    
+    public static void main(String[] args) {
+    }    
+        private double altura;
+        private int sexo;
+        private double pesoAtual;
+        
+        public Peso(){
+        }
+   
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPesoAtual() {
+        return pesoAtual;
+    }
+
+    public void setPesoAtual(double pesoAtual) {
+        this.pesoAtual = pesoAtual;
+    }
+    
+        
+    public double calcularPesoIdeal(){
+        //Peso ideal para Homens(1): (72.7 * altura) – 58;
+       //Peso ideal para Mulheres(2): (62.1 * altura) – 44.7;
+        
+       double pesoIdeal=0;
+        if(this.sexo == 1){
+        pesoIdeal =((72.7*this.altura)-58);
+            
+        }else if (this.sexo == 2){
+        pesoIdeal =((62.1*this.altura)-44.7);
+            
+        }
+        return pesoIdeal;
+    }
+    
+    public String verificarPeso(){
+    /*“Abaixo do peso” se seu peso atual for menor que o peso ideal -2.
+     *Estará “Acima do peso” se seu peso atual for maior que o peso ideal + 2.
+     *Caso nenhuma das opções acima for verdadeira, estará no peso ideal. */
+    
+       String r1= "";
+       
+        if (pesoAtual < (calcularPesoIdeal() - 2 )){
+            r1 = "Você está abaixo do peso ideal";    
+            return r1;
+            
+        }else if (pesoAtual > (calcularPesoIdeal()+2)){
+            r1 = "Você está acima do peso ideal";
+            return r1;
+        }else {
+            r1 = "Vccê está no peso ideal ";
+            return r1;
+        }    
+    }    
+      
+  
+    public String toString(){
+        return " Peso Ideal: " +calcularPesoIdeal() +
+         "\n Analise do peso atual: \n " + verificarPeso();        
+        }
+   
+    }
+       
+    
+    
+    
+        
+        
+        
+   
+    
+
